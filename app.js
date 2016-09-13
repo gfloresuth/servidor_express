da146e17b621a2b55bebc0838fa0fda29435dbd1
Gustavo Adolfo Flores González <Gustavo Adolfo Flores Gonzlez>
2016-09-13 06:56:24 -0700

    Primeros archivos

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+
+const PORT=8090;
+
+app.engine('html', require('atpl').__express);
+app.set('view engine','html');
+app.set('devel',false);
+// views
+
+app.get('/nosotros/',function(req,res){
+    res.render('nosotros',{'nombre':'Patito plus'});
+});
+
+
+app.get('/',function(req,res){
+    res.send('Hola que tal');
+});
+
+app.listen(PORT, function(){
+    console.log('Listo');
+});
